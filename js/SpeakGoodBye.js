cb6b089df2b645df809380860549696ccdb69328
Vamsi <vamsi_skillmail@yahoo.com>
2017-01-01 18:36:25 -0800

    Assignment 4 completed

---
+++ js/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function () {
+var speakWord = "Good Bye";
+var byeSpeaker = {};
+
+byeSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+window.byeSpeaker = byeSpeaker
+
+})(window);
